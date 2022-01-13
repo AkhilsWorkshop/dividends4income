d690fc491a34f61cfbbf3c1a179839e2eec30c91
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-01-12 22:13:28 -0600

    modified path

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'DjangoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['D:/Github/Django-Stocks/stocks'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
