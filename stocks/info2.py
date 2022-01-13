363480d5574fb4fc63cfe92ae854bff540e8cfb8
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-01-13 14:51:18 -0600

    Removed yf api

--- stocks/info2.py
+++ stocks/info2.py
@@ -1,13 +1,13 @@
-import requests
+#import requests
 
-headers = {
-    'x-rapidapi-host': "twelve-data1.p.rapidapi.com",
-    'x-rapidapi-key': "940dcf091fmsh54503cb692d31e0p16b3e2jsnb6f10c1fb3f4"
-}
+#headers = {
+  #  'x-rapidapi-host': "twelve-data1.p.rapidapi.com",
+   # 'x-rapidapi-key': "940dcf091fmsh54503cb692d31e0p16b3e2jsnb6f10c1fb3f4"
+#}
 
-def twelve_data_profile(ticker):
-    url = "https://twelve-data1.p.rapidapi.com/profile"
-    querystring = {"symbol":ticker}
-    response = requests.get(url, headers=headers, params=querystring)
-    return response.json()
+#def twelve_data_profile(ticker):
+  #  url = "https://twelve-data1.p.rapidapi.com/profile"
+  #  querystring = {"symbol":ticker}
+  #  response = requests.get(url, headers=headers, params=querystring)
+  #  return response.json()
 
--- stocks/views.py
+++ stocks/views.py
@@ -1,13 +1,9 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from pandas.core.frame import DataFrame
 from .forms import TickerForm
 from .dividends import company_dividend
-from .info import company_info
 from .news import company_news
-from .info2 import twelve_data_profile
-from .history import twelve_data_stats
 from .info_all import yfinance_info
 
 @csrf_exempt
@@ -26,9 +22,9 @@ def ticker(request, tid):
     context['ticker'] = tid
     context['news'] = company_news(tid)
     context['dividend'] = company_dividend(tid)
-    context['info'] = company_info(tid)
+    #context['info'] = company_info(tid)
     #context['info2'] = twelve_data_profile(tid)
-    context['stats'] = twelve_data_stats(tid)
+    #context['stats'] = twelve_data_stats(tid)
     context['infoall'] = yfinance_info(tid)
     return render(request, 'ticker.html', context)
 
