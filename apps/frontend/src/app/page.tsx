b420e4edbb420a7d5e23b6c86d45312d8b18a6a7
Akhil <hakhilkumar@gmail.com>
2026-03-17 15:32:23 +0530

    Remove revalidation export from HomePage component

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 import { HomepageLayout } from '@/components/Homepage/Layout'
 import { fetchPopularStocks } from '@/lib/api'
 
-export const revalidate = 1800
-
 export default async function HomePage() {
 
     const popularStocksData = await fetchPopularStocks()
